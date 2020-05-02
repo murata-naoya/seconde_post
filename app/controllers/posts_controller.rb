5ef52f56628649af948a55d31b05256cd5d12891
murata-naoya <murata.small.baseball@gmail.com>
2020-05-02 05:12:56 +0000

    fix_token

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,23 @@ class PostsController < ApplicationController
     post.title = params[:title]
     post.channel = params[:channel]
     post.body = params[:body]
-    notifier = Slack::Notifier.new(ENV['SLACK_API'],
-      channel: '#tech',
-      user_name: 'ELITE CAMP事務局',
-    )
-    attachments = {
-      fallback: "",
-      title: "【#{post.title}】",
-      text: "#{post.body}",
-      color: 'good'
-    }
-    notifier.post text: "<!channel>\n", attachments: attachments
-    redirect_to post_done_path
+    # if post.title.present? && post.channel.present? && post.body.present?
+      notifier = Slack::Notifier.new(ENV['SLACK_API'],
+        channel: '#tech',
+        user_name: 'ELITE CAMP事務局',
+      )
+      attachments = {
+        fallback: "",
+        title: "【#{post.title}】",
+        text: "#{post.body}",
+        color: 'good'
+      }
+      notifier.post text: "<!channel>\n", attachments: attachments
+      redirect_to post_done_path
+    # else
+      # flash[:notice] = "投稿できませんでした。内容を確認してください。"
+      # render "new"
+    # end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
+  root :to => "posts#new"
+  post "posts" => "posts#create", as: "posts"
   get "done" => "posts#done", as: "post_done"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
